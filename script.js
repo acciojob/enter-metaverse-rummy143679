c4b6ecc0906996260cc10a9df5d8351be24c409b
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-19 22:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById("enterBtn");
-const statusElement = document.getElementById("status");
+const container = document.getElementById("status");
 
 btn.addEventListener("click", () => {
   const h1 = document.createElement("h1");
   h1.textContent = "Entered Metaverse";
-  h1.id = "status"; // maintain the same id
-  statusElement.replaceWith(h1);
+
+  container.innerHTML = "";        // Clear the container
+  container.appendChild(h1);       // Add the new <h1> inside the container
 });
