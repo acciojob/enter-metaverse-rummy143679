949939a2a66a72afde630e7683947ec6f604e900
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-19 21:52:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //your JS code here. If required.
 const pt = document.getElementById("status");
 const btn = document.getElementById("enterBtn");
-const body = document.getElementById("body");
 
 btn.addEventListener("click",() => {
 	const ele = document.createElement("h1");
 	ele.textContent = "Entered Metaverse";
-	ele.setAttribute("id","status");
+	pt.innerHtml = ele;
 	pt.replaceWith(ele);
 	
 })
