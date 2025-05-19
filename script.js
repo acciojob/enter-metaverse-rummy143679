7807d79975987b0ce3baa811618737aed2f6985b
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-19 21:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const pt = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click",() => {
+	pt.textContent = "Entered Metaverase";
+})
